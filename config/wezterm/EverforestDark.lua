ed4f1189405334e8bee148e02844edf603f1e29b
Frusadev <frusadev@gmail.com>
2024-05-13 20:45:40 +0000

    Added wezterm config

---
+++ config/wezterm/EverforestDark.lua
@@ -0,0 +1,16 @@
+return {
+  foreground = "#d8dee9",
+  background = "#1b1c1e",
+  cursor_bg = "#61afef",
+  cursor_border = "#61afef",
+  selection_bg = "#3e4451",
+  selection_fg = "#d8dee9",
+  ansi = {
+    "#282c34", "#e06c75", "#98c379", "#e5c07b",
+    "#61afef", "#c678dd", "#56b6c2", "#d8dee9"
+  },
+  brights = {
+    "#5c6370", "#e06c75", "#98c379", "#e5c07b",
+    "#61afef", "#c678dd", "#56b6c2", "#ffffff"
+  },
+}
